53c3a712fbf8069199ff395534a77bff58a0a023
ramneek008 <singhramneek274@gmail.com>
2021-01-29 23:03:56 +0530

    game implementation

---
+++ index.js
@@ -0,0 +1,63 @@
+const cards = document.querySelectorAll(".card");
+// console.log(cards);
+cards.forEach((card) => {
+	card.addEventListener("click", flip);
+})
+
+var isFlipped = false;
+var firstCard;
+var seecondCard;
+
+function flip(){
+	//console.log(this);
+	this.classList.add("flip");
+	if(!isFlipped)
+	{
+		isFlipped=true;
+		firstCard=this;
+	}
+	else
+	{
+		secondCard=this;
+		checkIt();
+	}
+}
+
+function checkIt(){
+	if(firstCard.dataset.image === secondCard.dataset.image)
+	{
+		success();
+	}
+	else
+	{
+		fail();
+	}
+}
+
+function success(){
+	firstCard.removeEventListener("click", flip);
+	secondCard.removeEventListener("click", flip);
+	reset();
+}
+
+function fail(){
+	setTimeout(() => {
+		firstCard.classList.remove("flip");
+		secondCard.classList.remove("flip");
+	},1000);
+}
+
+function reset(){
+	isFlipped=false;
+	firstCard=null;
+	secondCard=null;
+}
+
+function shuffle(){
+	cards.forEach((card) => {
+		var index = Math.floor(Math.random() * 16);
+		card.style.order = index;
+	})
+}
+
+shuffle();
